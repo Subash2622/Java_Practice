74a3afb343147b3a24c3a5f1062a4ead7438725f
Subash2622 <subash9938sahoo@gmail.com>
2025-06-11 15:48:07 +0530

    11-06-2025

---
+++ MyFirstProject/src/OOPS/Inheritance/Employee.java
@@ -0,0 +1,47 @@
+package OOPS.Inheritance;
+
+
+import java.math.BigDecimal;
+
+public class Employee extends Person{
+    private String title;
+    private String employerName;
+    private char employerGrade;
+    private BigDecimal Salary;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEmployerName() {
+        return employerName;
+    }
+
+    public void setEmployerName(String employerName) {
+        this.employerName = employerName;
+    }
+
+    public char getEmployerGrade() {
+        return employerGrade;
+    }
+
+    public void setEmployerGrade(char employerGrade) {
+        this.employerGrade = employerGrade;
+    }
+
+    public BigDecimal getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        Salary = salary;
+    }
+
+    public String toString(){
+        return super.toString() + title + "#" + employerName + "#" + employerGrade;
+    }
+}
--- MyFirstProject/src/OOPS/Inheritance/StudentRunner.java
+++ MyFirstProject/src/OOPS/Inheritance/StudentRunner.java
@@ -2,10 +2,22 @@ package OOPS.Inheritance;
 
 public class StudentRunner {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.setName("Subash");
-        student.setEmail("test@gmail.com");
+//        Student student = new Student();
+//        student.setName("Subash");
+//        student.setEmail("test@gmail.com");
+//
+//        System.out.println(student);
 
-        System.out.println(student);
+//        Person person = new Person();
+//        String value = person.toString();
+//        System.out.println(value);
+//        System.out.println(person);
+        Employee employee = new Employee();
+        employee.setName("Subash Sahoo");
+        employee.setEmail("test@gmail.com");
+        employee.setEmployerGrade('A');
+        employee.setTitle("Programmer Analyst");
+
+        System.out.println(employee);
     }
 }
